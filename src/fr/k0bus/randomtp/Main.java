4e1b57d4d5630cf2c295d065aa3a3a79cc6181d5
Brad Van Dyne <pyropyro78@gmail.com>
2019-10-17 19:10:29 -0700

    added forgotten files to finish adding the new config system

--- src/fr/k0bus/randomtp/Main.java
+++ src/fr/k0bus/randomtp/Main.java
@@ -19,13 +19,11 @@ public class Main extends JavaPlugin {
 	@Override
 	public void onEnable()
 	{
-		this.saveDefaultConfig();
-		Main.config = this.getConfig();
-		Main.tag = ChatColor.translateAlternateColorCodes('&', Main.config.getString("tag") + "&r ");
+		ConfigManager.onServerEnable();
+		Main.tag = ChatColor.translateAlternateColorCodes('&', MainConfig.tag + "&r ");
 		Main.plugin = this;
 		this.getCommand("randomtp").setExecutor(new CommandRandomTP());
 		this.getCommand("rtp_debug").setExecutor(new CommandDebug());
-		this.setDefaultConfig();
 		if(Main.plugin.getServer().getPluginManager().getPlugin("Lands")!=null)
 		{
 			landsaddon = new LandsAddonsPlugins(Main.plugin);
@@ -34,22 +32,7 @@ public class Main extends JavaPlugin {
 	@Override
 	public void onDisable()
 	{
-		this.saveConfig();
-	}
-	
-	private void setDefaultConfig()
-	{
-		Main.config.addDefault("tag", "&r[&cRandomTP&r]");
-		Main.config.addDefault("distance.min", 200);
-		Main.config.addDefault("distance.max", 1000);
-		Main.config.addDefault("max-try", 50);
-		Main.config.addDefault("delay.teleport", 5);
-		Main.config.addDefault("delay.cooldown", 360);
-		//Exception plugins
-		Main.config.addDefault("plugins.lands", true);
-		Main.config.addDefault("plugins.worldguard", true);
-		Main.config.options().copyDefaults(true);
-		this.saveConfig();
+		
 	}
 	
 }
