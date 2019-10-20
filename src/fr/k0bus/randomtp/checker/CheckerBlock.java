27346a4f04396418c7c5f658d270dc3256f29e39
K0bus <k0bus@mail.com>
2019-10-21 01:31:40 +0200

    Debug pom.xml / Add Maven/plugin.yml implement

--- src/fr/k0bus/randomtp/checker/CheckerBlock.java
+++ src/fr/k0bus/randomtp/checker/CheckerBlock.java
@@ -1,9 +1,19 @@
 package fr.k0bus.randomtp.checker;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import fr.k0bus.randomtp.Main;
+import fr.k0bus.randomtp.commands.CommandDebug;
 
 public class CheckerBlock {
+	
+	CheckerPlugins cp = new CheckerPlugins();
+
 	public Location getSafeLocation(Location location)
 	{
 		Block b = location.getWorld().getHighestBlockAt(location);
@@ -12,5 +22,29 @@ public class CheckerBlock {
 				return b.getLocation();
 		}
 		return null;
+	}
+
+	public boolean onCommand(CommandDebug commandDebug, CommandSender sender, Command command, String label, String[] args)
+	{
+		if(sender instanceof Player && args.length > 0)
+		{
+			Player player = (Player) sender;
+			if(player.hasPermission("randomtp.debug"))
+			{
+				if(args[0].contains("plc"))
+				{
+					player.sendMessage(Main.tag + ChatColor.LIGHT_PURPLE + "Vérification des claims...");
+					if(cp.isClaimed(player.getLocation()))
+					{
+						player.sendMessage(Main.tag + ChatColor.DARK_RED + "Cette endroit est protégé par un claim !");
+					}
+					else
+					{
+						player.sendMessage(Main.tag + ChatColor.DARK_GREEN + "Cette endroit n'est pas protégé par un claim !");
+					}
+				}
+			}
+		}
+		return true;
 	}
 }
--- src/fr/k0bus/randomtp/checker/CheckerPlugins.java
+++ src/fr/k0bus/randomtp/checker/CheckerPlugins.java
@@ -1,6 +1,10 @@
 package fr.k0bus.randomtp.checker;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import fr.k0bus.randomtp.Main;
 import fr.k0bus.randomtp.plugins.WorldGuardPlugins;
@@ -19,5 +23,29 @@ public class CheckerPlugins {
 			claimed = new WorldGuardPlugins().isClaimed(location);
 		}
 		return claimed;
+	}
+
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+	{
+		if(sender instanceof Player && args.length > 0)
+		{
+			Player player = (Player) sender;
+			if(player.hasPermission("randomtp.debug"))
+			{
+				if(args[0].contains("plc"))
+				{
+					player.sendMessage(Main.tag + ChatColor.LIGHT_PURPLE + "Vérification des claims...");
+					if(isClaimed(player.getLocation()))
+					{
+						player.sendMessage(Main.tag + ChatColor.DARK_RED + "Cette endroit est protégé par un claim !");
+					}
+					else
+					{
+						player.sendMessage(Main.tag + ChatColor.DARK_GREEN + "Cette endroit n'est pas protégé par un claim !");
+					}
+				}
+			}
+		}
+		return true;
 	}
 }
